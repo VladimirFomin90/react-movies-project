39ac3e14eb6dbee49869c8412b20f52653ee70c3
VladimirFomin90 <vladimirfomin90@yandex.ru>
2022-09-19 23:25:30 +0600

    create my first react app

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="blue-grey">
+                <div className="nav-wrapper">
+                    <a href="#" className="brand-logo">My favorite movies</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="#">repository</a></li>
+                    </ul>
+                </div>
+            </nav>
+}
+
+export {Header}
